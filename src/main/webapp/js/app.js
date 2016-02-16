6c131cbe6f6a12e14ba873a998bdfe70503b8b08
fysuy <uy.sgonzalez@gmail.com>
2016-02-15 23:36:01 -0300

    fix event preventDefault cancelaba teclas en la pagina

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -13,21 +13,24 @@
 //		});
 		
 		$('body').keydown(function(e){
-			event.preventDefault();
 			s = Snap("#svg");
 			var galleon = s.select("#ship");
 			var coord = galleon.getBBox();
 			switch(e.which){
 				case 38: 
+					event.preventDefault();
 					galleon.animate({ y: coord.y - 100 }, 2000);
 					break;
 				case 40: 
+					event.preventDefault();
 					galleon.animate({ y: coord.y + 100 }, 2000);
 					break;
 				case 39: 
+					event.preventDefault();
 					galleon.animate({ x: coord.x + 100 }, 2000);
 					break;
 				case 37: 
+					event.preventDefault();
 					galleon.animate({ x: coord.x - 100 }, 2000);
 					break;
 			}
