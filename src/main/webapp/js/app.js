62a4bf14a710bf41b9cb09136682a5d64abf1fae
fysuy <uy.sgonzalez@gmail.com>
2016-02-21 18:12:53 -0300

    barquito moviendose

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,70 +1,95 @@
 var appJs = (function  () {
-  var $container = $(".game-container");
   var game = new Phaser.Game(800, 600, Phaser.AUTO, "game-container", { preload: preload, create: create, update: update });
 
-  var ocean;
-  var submarine;
-  var shadow;
+  var ocean, port, submarine;
 
   var currentSpeed = 0;
 
   function preload() {
     game.load.image('ocean', 'assets/pattern-land.png');
+    game.load.image('port', 'assets/port.png');
     game.load.image('submarine', 'assets/ship-grey.png');
   }
 
   function create() {
-    game.world.setBounds(-1000, -1000, 2000, 2000);
+    game.world.setBounds(0, 0, 2000, 2000);
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.scale.refresh();
 
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
     ocean = game.add.tileSprite(0, 0, 800, 600, 'ocean');
     ocean.fixedToCamera = true;
 
-    submarine = game.add.sprite(0, 0, 'submarine', 'submarine');
+    submarine = game.add.sprite(100, 300, 'submarine');
     submarine.anchor.setTo(0.5, 0.5);
-    submarine.animations.add('move', ['submarine'], 20, true);
+    game.physics.enable(submarine, Phaser.Physics.ARCADE);
+    submarine.body.collideWorldBounds = true;
+    //submarine.body.bounce.setTo(1, 1);
+
+    port = game.add.sprite(0, 0, 'port');
+    game.physics.enable(port, Phaser.Physics.ARCADE);
+    port.body.collideWorldBounds = true;
+    //port.body.immovable = true;
+
+    //submarine.animations.add('move', ['submarine'], 20, true);
 
     //  This will force it to decelerate and limit its speed
-    game.physics.enable(submarine, Phaser.Physics.ARCADE);
-    submarine.body.drag.set(0.2);
-    submarine.body.maxVelocity.setTo(400, 400);
-    submarine.body.collideWorldBounds = true;
+    
+    //submarine.body.drag.set(0.2);
+    //submarine.body.maxVelocity.setTo(400, 400);
+    
 
     game.camera.follow(submarine);
-    game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300);
+    //game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300);
     game.camera.focusOnXY(0, 0);
 
     cursors = game.input.keyboard.createCursorKeys();
   }
 
   function update() {
+    //game.physics.arcade.overlap(port, submarine, null, null, this);
+    game.physics.arcade.collide(port, submarine);
+
+    // if (cursors.left.isDown)
+    // {
+    //   currentSpeed = 300;
+    //   if ((submarine.angle > 0) && (submarine.angle < 90)) {
+    //     submarine.angle += 4;   
+    //   }
+    // }
+    // else if (cursors.right.isDown)
+    // {
+    //   currentSpeed = 300;
+    //   submarine.angle += 4;
+    // } else {
+    //     if (currentSpeed > 0)
+    //     {
+    //       currentSpeed -= 4;
+    //     }
+    // }
+
     if (cursors.left.isDown)
     {
-        submarine.angle -= 4;
+        submarine.x -= 4;
     }
     else if (cursors.right.isDown)
     {
-        submarine.angle += 4;
+        submarine.x += 4;
     }
-
-    if (cursors.up.isDown)
+    else if (cursors.down.isDown)
     {
-        //  The speed we'll travel at
-        currentSpeed = 300;
+        submarine.y += 4;
     }
-    else
+    else if (cursors.up.isDown)
     {
-        if (currentSpeed > 0)
-        {
-            currentSpeed -= 4;
-        }
+        submarine.y -= 4;
     }
 
     if (currentSpeed > 0)
     {
-        game.physics.arcade.velocityFromRotation(submarine.rotation, currentSpeed, submarine.body.velocity);
+      game.physics.arcade.velocityFromRotation(submarine.rotation, currentSpeed, submarine.body.velocity);
     }
 
     ocean.tilePosition.x = -game.camera.x;
